666ee1fdcab2c90a9dccb34dffcac5a01918cc45
Sebastian Lyng Johansen <sebastian@lyngjohansen.com>
2024-01-20 09:55:37 +0100

    Continue refactor

--- lua/blame.lua
+++ lua/blame.lua
@@ -24,72 +24,55 @@ end
 
 ---@param blame_type "window"|"virtual"|""
 local function open(blame_type)
-    local is_window_open = window_blame.is_window_open()
-    local is_virtual_open = virtual_blame.nsId ~= nil
+    local filename = vim.api.nvim_buf_get_name(0)
+    local cwd = vim.fn.expand("%:p:h")
 
-    if is_window_open or is_virtual_open then
-        return
-    else
-        local filename = vim.api.nvim_buf_get_name(0)
-        local cwd = vim.fn.expand("%:p:h")
+    vim.system({ "git", "--no-pager", "blame", "--line-porcelain", filename }, {
+        cwd = cwd,
+    }, function(out)
+        if out.code ~= 0 then
+            return vim.notify("Could not execute git blame")
+        end
 
-        vim.system({ "git", "--no-pager", "blame", "--line-porcelain", filename }, {
-            cwd = cwd,
-        }, function(out)
-            if out.code ~= 0 then
-                vim.notify("Could not execute git blame", vim.log.levels.INFO)
-                return
+        local output = {}
+        local stdout = out.stdout:sub(-1) == "\n" and out.stdout or out.stdout .. "\n"
+        for k in stdout:gmatch("([^\n]*)\n") do
+            table.insert(output, k)
+        end
+
+        vim.schedule(function()
+            local parsed_blames = blame_parser.parse_porcelain(output)
+            highlights.map_highlights_per_hash(parsed_blames)
+
+            local line_strings = blame_parser.format_blame_to_line_string(parsed_blames, M.config)
+            if blame_type == "window" or blame_type == "" then
+                window_blame.window_blame(line_strings, M.config)
+            elseif blame_type == "virtual" then
+                virtual_blame.virtual_blame(parsed_blames, M.config)
             end
-
-            local output = {}
-            local stdout = out.stdout:sub(-1) == "\n" and out.stdout or out.stdout .. "\n"
-            for k in stdout:gmatch("([^\n]*)\n") do
-                table.insert(output, k)
-            end
-
-            vim.schedule(function()
-                local parsed_blames = blame_parser.parse_porcelain(output)
-                highlights.map_highlights_per_hash(parsed_blames)
-
-                local line_strings = blame_parser.format_blame_to_line_string(parsed_blames, M.config)
-                if blame_type == "window" or blame_type == "" then
-                    window_blame.window_blame(line_strings, M.config)
-                elseif blame_type == "virtual" then
-                    virtual_blame.virtual_blame(parsed_blames, M.config)
-                end
-            end)
         end)
-    end
+    end)
 end
 
-local function close()
+vim.api.nvim_create_user_command("ToggleBlame", function(args)
     local is_window_open = window_blame.is_window_open()
     local is_virtual_open = virtual_blame.nsId ~= nil
-
-    if is_window_open then
-        return window_blame.close_window()
-    elseif is_virtual_open then
-        return virtual_blame.close_virtual()
-    end
-end
-
-M.toggle = function(arguments)
-    local is_window_open = window_blame.is_window_open()
-    local is_virtual_open = virtual_blame.nsId ~= nil
+    local arg = args["args"]
 
     if is_window_open or is_virtual_open then
-        return close()
+        if is_window_open then
+            return window_blame.close_window()
+        elseif is_virtual_open then
+            return virtual_blame.close_virtual()
+        end
     else
-        return open(arguments["args"])
+        return open(arg)
     end
-end
-
-M.enable = function(arguments)
-    return open(arguments["args"])
-end
-
-M.disable = function()
-    return close()
-end
+end, {
+    nargs = "?",
+    complete = function(_, _)
+        return { "virtual", "window" }
+    end,
+})
 
 return M
